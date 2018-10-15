27b0587f386a866b6dddee4a0edf4b7760a7d7c4
Syzygeezus <percyman99games@gmail.com>
2018-10-15 15:26:41 +1300

    15:26 181015

---
+++ showrole.js
@@ -0,0 +1,7 @@
+function ShowInspect(){
+  document.getElementById('role-inspected').className = "inspect-showrole";
+}
+
+function HideInspect(){
+  document.getElementById('role-inspected').className = "inspect-hidden";
+}
